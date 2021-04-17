bfefb0a351760089541db84ab60c95236979cd53
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2021-04-18 01:32:05 +0500

    done with the project 4A of BOOTCAMP 2020

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
-// import './App.css';
-import RedQueen from './Red Queen Race/index';
+import useWebAnimations from '@wellyshen/use-web-animations';
 
-const App = () =>{
-  return(
-    <div>
-     <RedQueen />
+import './App.css';
+
+import Cartoon from './assets/cartoon.gif';
+
+function App() {
+  const { ref, getAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(-100%,0)' },
+      { transform: 'translate(570%,0)' }
+    ],
+    timing: {
+      duration: 5000,
+      playbackRate: 1,
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  });
+
+  const speedUpCartoon = () => {
+    const animation = getAnimation();
+    animation.updatePlaybackRate(animation.playbackRate * 1.1);
+    setInterval(
+      () => {
+        if (animation.playbackRate > 1) {
+          animation.playbackRate *= .9;
+        }
+      }, 6000)
+  };
+
+  return (
+    <div className="wrapper" onClick={speedUpCartoon}>
+      <img src={Cartoon} className="cartoon" ref={ref} alt="Main Character" />
     </div>
-  )
+  );
 }
-
 
 export default App;
